5bf2177079b3b1a4522b59cfe3f6258274279b0c
William Wedler <wwedler@riseup.net>
2015-08-25 23:19:02 -0400

    customizing emacs backup settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -123,7 +123,8 @@
 
 (define-key projectile-mode-map [?\s-d] 'helm-projectile-find-dir)
 (define-key projectile-mode-map [?\s-p] 'helm-projectile-switch-project)
-(define-key projectile-mode-map [?\s-f] 'helm-projectile-find-file)
+;(define-key projectile-mode-map [?\s-f] 'helm-projectile-find-file)
+(define-key projectile-mode-map [M-p] 'helm-projectile-find-file)
 (define-key projectile-mode-map [?\s-s] 'helm-do-ag-project-root)
 
 (tabbar-mode)
@@ -153,3 +154,22 @@
 (add-hook 'web-mode-hook 'my-web-mode-hook)
 (set-face-attribute 'web-mode-symbol-face nil :foreground "SeaGreen")
 
+
+
+
+
+;; Customizing backup settings
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
