1b6d1689aeb0e8f2b3f97fa846fc911de2831334
William Wedler <william.wedler@resquared.com>
2021-06-03 09:51:00 -0400

    adding god-mode with some configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -905,6 +905,35 @@
 ;;   always split horizontal: 0
 
 
+; Set up god-mode for modal editing
+; See also https://github.com/jmorag/kakoune.el
+(straight-use-package 'god-mode)
+;; (setq god-exempt-major-modes nil)
+;; (setq god-exempt-predicates nil)
+(setq god-mode-enable-function-key-translation nil)
+(require 'god-mode)
+(global-set-key (kbd "<escape>") #'god-local-mode)
+(god-mode)
+; https://emacs.zdx.cat/#org16c18e2
+; (defun my-god-mode-update-cursor-type ()
+;  (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+; (add-hook 'post-command-hook #'my-god-mode-update-cursor-type)
+(add-hook 'god-mode-enabled-hook '(lambda ()
+                                    (setq cursor-type '(hbar . 8))))
+(add-hook 'god-mode-disabled-hook '(lambda ()
+                                     (setq cursor-type 'box)))
+(define-key god-local-mode-map (kbd "i") #'god-local-mode)
+(define-key god-local-mode-map (kbd ".") #'repeat)
+;; god-mode is greedy and tries to prefix everything with C- so
+;; modify some common keybindings to work better in god-mode.
+(define-key god-local-mode-map (kbd "C-x C-1") #'delete-other-windows)
+(define-key god-local-mode-map (kbd "C-x C-2") #'split-window-below)
+(define-key god-local-mode-map (kbd "C-x C-3") #'split-window-right)
+(define-key god-local-mode-map (kbd "C-x C-0") #'delete-window)
+(define-key god-local-mode-map (kbd "C-x C-o") #'other-window)
+(define-key god-local-mode-map (kbd "C-x C-b") #'ivy-switch-buffer)
+(define-key god-local-mode-map (kbd "C-x C-c") #'list-buffers) ;; Re-bind to prevent accidental exit
+
 
 ;; (global-set-key (kbd "M-<f6>") nil)
 (global-set-key (kbd "M-<f6>") 'imenu)
